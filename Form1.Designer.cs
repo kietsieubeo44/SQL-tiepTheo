56603759f656bb5312eded14865042c36f8a7266
kiet <kietsieubeo44@gmail.com>
2023-11-01 15:58:06 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace QuanLyKhoaApp
+{
+    partial class FormQuanLyKhoa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labMaKhoa = new MetroFramework.Controls.MetroLabel();
+            this.LabelTenKhoa = new MetroFramework.Controls.MetroLabel();
+            this.btnThem = new MetroFramework.Controls.MetroButton();
+            this.btnXoa = new MetroFramework.Controls.MetroButton();
+            this.btnSua = new MetroFramework.Controls.MetroButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtMaKhoa = new System.Windows.Forms.TextBox();
+            this.txtTenKhoa = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labMaKhoa
+            // 
+            this.labMaKhoa.AutoSize = true;
+            this.labMaKhoa.Location = new System.Drawing.Point(42, 25);
+            this.labMaKhoa.Name = "labMaKhoa";
+            this.labMaKhoa.Size = new System.Drawing.Size(63, 20);
+            this.labMaKhoa.TabIndex = 0;
+            this.labMaKhoa.Text = "Ma Khoa";
+            // 
+            // LabelTenKhoa
+            // 
+            this.LabelTenKhoa.AutoSize = true;
+            this.LabelTenKhoa.Location = new System.Drawing.Point(42, 74);
+            this.LabelTenKhoa.Name = "LabelTenKhoa";
+            this.LabelTenKhoa.Size = new System.Drawing.Size(65, 20);
+            this.LabelTenKhoa.TabIndex = 2;
+            this.LabelTenKhoa.Text = "Ten khoa";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(43, 240);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 4;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseSelectable = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(181, 240);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 5;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseSelectable = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(317, 240);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 6;
+            this.btnSua.Text = "sua";
+            this.btnSua.UseSelectable = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(481, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(298, 296);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtMaKhoa
+            // 
+            this.txtMaKhoa.Location = new System.Drawing.Point(154, 25);
+            this.txtMaKhoa.Name = "txtMaKhoa";
+            this.txtMaKhoa.Size = new System.Drawing.Size(270, 22);
+            this.txtMaKhoa.TabIndex = 8;
+            this.txtMaKhoa.TextChanged += new System.EventHandler(this.txtMaKhoa_TextChanged);
+            // 
+            // txtTenKhoa
+            // 
+            this.txtTenKhoa.Location = new System.Drawing.Point(154, 74);
+            this.txtTenKhoa.Name = "txtTenKhoa";
+            this.txtTenKhoa.Size = new System.Drawing.Size(270, 22);
+            this.txtTenKhoa.TabIndex = 9;
+            // 
+            // FormQuanLyKhoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(791, 333);
+            this.Controls.Add(this.txtTenKhoa);
+            this.Controls.Add(this.txtMaKhoa);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.LabelTenKhoa);
+            this.Controls.Add(this.labMaKhoa);
+            this.Name = "FormQuanLyKhoa";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel labMaKhoa;
+        private MetroFramework.Controls.MetroLabel LabelTenKhoa;
+        private MetroFramework.Controls.MetroButton btnThem;
+        private MetroFramework.Controls.MetroButton btnXoa;
+        private MetroFramework.Controls.MetroButton btnSua;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtMaKhoa;
+        private System.Windows.Forms.TextBox txtTenKhoa;
+    }
+}
+
